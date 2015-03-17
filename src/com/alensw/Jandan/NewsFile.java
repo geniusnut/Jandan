d55d1bc3e81d2ae8a79a0acdc3ff7683f83ec675
yw07 <yw07@q-supreme.com>
2015-03-17 18:17:51 +0800

    NewsFIle

---
+++ src/com/alensw/Jandan/NewsFile.java
@@ -0,0 +1,45 @@
+package com.alensw.Jandan;
+
+import android.content.Context;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+/**
+ * Created by yw07 on 15-3-17.
+ */
+public class NewsFile extends ListFile<News> {
+	public static final String NES_FILE_NAME = "news.bin";
+
+	public boolean load(Context context, String name) {
+		return load(context.getFileStreamPath(name));
+	}
+
+	@Override
+	public String getTypeName() {
+		return "News";
+	}
+
+	@Override
+	public News readEntry(DataInputStream dis) throws Throwable {
+		final News news = new News();
+		news.mLink = dis.readUTF();
+		news.mTitle = dis.readUTF();
+		news.mCover = dis.readUTF();
+		news.mAuthor = dis.readUTF();
+		news.mTag = dis.readUTF();
+		news.mCont = dis.readInt();
+		return null;
+	}
+
+	@Override
+	public void writeEntry(DataOutputStream dos, News news) throws Throwable {
+
+		dos.writeUTF(news.mLink);
+		dos.writeUTF(news.mTitle);
+		dos.writeUTF(news.mCover);
+		dos.writeUTF(news.mAuthor);
+		dos.writeUTF(news.mTag);
+		dos.writeInt(news.mCont);
+	}
+}
