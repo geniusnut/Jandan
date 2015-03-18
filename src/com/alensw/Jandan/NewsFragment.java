4113349c90935d6c1f5e2cf3174f420c1465dfc6
yw07 <yw07@q-supreme.com>
2015-03-18 17:58:42 +0800

    Revert "clear project property"
    This reverts commit b326e26674f5741013f794403d010d10d240a1f7.

--- src/com/alensw/Jandan/NewsFragment.java
+++ src/com/alensw/Jandan/NewsFragment.java
@@ -206,7 +206,7 @@ public class NewsFragment extends Fragment {
 
 
 				//viewHolder.badge.setBadgeMargin(100);
-				Drawable iconCircle = SVG.getDrawable(getResources(), R.raw.ic_dot,
+				Drawable iconCircle = SVG.getDrawable(getResources(), R.raw.ic_button_radio_on,
 						0xCCFF0000 | 0xc0000000);
 				viewHolder.badge.setBackgroundDrawable(iconCircle);
 
